5df291d6c7395a45667654e5cf65c4663ea2be6b
erhan turuncu <rohatturuncu@gmail.com>
2021-02-05 01:36:33 +0300

    5.02.2021

--- MyDictionary/MyDictionary.cs
+++ MyDictionary/MyDictionary.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace MyDictionary
 {
-    class MyDictionary
+    class MyDictionary<K,V>
     {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            //Buraya keylerin eşsiz olması için kontrol yapısı gelecek!
+
+            K[] tempKeyArray = keys;
+            V[] tempValueArray = values;
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeyArray.Length; i++)
+            {
+                keys[i] = tempKeyArray[i];
+                values[i] = tempValueArray[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public void Count()
+        {
+            Console.WriteLine(keys.Length);           
+        }
+
+        public void ShowList()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Id:" + keys[i] + " Name: " + values[i]);
+            }
+        }
     }
 }
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 
 namespace MyDictionary
 {
@@ -6,12 +9,30 @@ namespace MyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> dizim1 = new Dictionary<int, string>();
+
+            dizim1.Add(1, "a");
+
+            MyDictionary<int, string> dizim = new MyDictionary<int, string>();
+
+            dizim.Add(1, "Nuri");
+            dizim.Add(2, "Bilge");
+            dizim.Add(3, "Ceylan");
+            dizim.Add(4, "İzel");
+            dizim.Add(5, "Çelik");
+            dizim.Add(6, "Ercan");
+            dizim.Add(7, "Mazhar");
+            dizim.Add(8, "Fuat");
+            dizim.Add(9, "Özkan");
+
+            Console.Write("Eleman Sayısı: ");
+            dizim.Count();
+            dizim.ShowList();
 
 
 
 
-            
+
 
 
 
